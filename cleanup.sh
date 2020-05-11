ba0d8a7e59d6ff7d27d70e72a0d771061c59bc57
Will Ezell <will@dotcms.com>
2020-05-11 11:29:50 -0400

    working

--- cleanup.sh
+++ cleanup.sh
@@ -3,5 +3,5 @@
 
 
 ./cleanup-01-undeploy.sh
-./cleanup-02-remove-prereq.sh
+./cleanup-02-delete-configmaps.sh
 ./cleanup-03-delete-cluster.sh
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ./setup-01-create-cluster.sh
-./setup-02-create-prereq.sh
+./setup-02-create-configmaps.sh
 ./setup-03-deploy.sh
